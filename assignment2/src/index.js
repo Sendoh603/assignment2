5863329aec1b591361747cf4988822966be3f774
Wei Zhang <sheepchina@hotmail.com>
2020-01-25 21:35:02 -0600

    Set up basic structure

---
+++ assignment2/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import Incomplete from './TodoList.js';
+
+ReactDOM.render(<Incomplete />, document.getElementById('root'));
