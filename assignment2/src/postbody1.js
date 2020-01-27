ab0c7872ed2018ae5a83e3ebae65b3536f5641ca
Wei Zhang <sheepchina@hotmail.com>
2020-01-27 03:15:00 -0600

    Add commit to postbody1.js

---
+++ assignment2/src/postbody1.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import picture from './image/albums/algorythm.jpeg';
+import images from './image/avatars/wei.jpeg';
+
+//this part is one of PostBody, click the up button to increse the voting counter through a handleClick function
+//to reset state
+class PostBody1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter:0, 
+      add () {
+        return this.counter + 1;
+      }
+    }
+  }
+  handleClick() {
+    let afterPlusNumber = this.state.add();
+    this.setState({counter:afterPlusNumber});
+  }
+  render () {
+
+    return (
+  
+      <div id="box">
+
+        <div id="leftContainer">
+          <img src={picture} id="albumsPicture" alt="haha"/>
+        </div>
+  
+        <div id="rightContainer">
+  
+          <button onClick={() =>this.handleClick()}>UP {this.state.counter}</button>
+          <div>Algorythm</div>
+          <div>Beyond Creation, 2018</div>
+          <div>
+            Submitted by:
+            <img src={images} id="submmittedPicture" alt="haha"/>
+          </div>
+  
+        </div>
+      
+      </div>
+        
+    );
+  }
+}
+
+export default PostBody1;
