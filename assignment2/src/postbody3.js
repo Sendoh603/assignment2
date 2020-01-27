cbc606c7dac1e50760b5041ec50c08c437061760
Wei Zhang <sheepchina@hotmail.com>
2020-01-27 02:55:35 -0600

    Set up a new postbody3.js file

---
+++ assignment2/src/postbody3.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import picture from './image/albums/redcloud.jpeg';
+import images from './image/avatars/wei.jpeg';
+class PostBody3 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter:0, 
+      add () {
+        return this.counter + 1;
+      }
+    }
+  }
+  handleClick() {
+    let afterPlusNumber = this.state.add();
+    this.setState({counter:afterPlusNumber});
+  }
+  render () {
+
+    return (
+  
+      <div id="box">
+
+        <div id="leftContainer">
+          <img src={picture} id="albumsPicture" alt="haha"/>
+        </div>
+  
+        <div id="rightContainer">
+  
+          <button onClick={() =>this.handleClick()}>UP {this.state.counter}</button>
+          <div>Under the Red Cloud</div>
+          <div>Amorphis, 2015</div>
+          <div>
+            Submitted by:
+            <img src={images} id="submmittedPicture" alt="haha"/>
+          </div>
+  
+        </div>
+      
+      </div>
+        
+    );
+  }
+}
+
+export default PostBody3;
