c629567c2d3ddd89ef906eebb4b83d51ebfdbae4
Wei Zhang <sheepchina@hotmail.com>
2020-01-26 22:03:00 -0600

    Add app.css to index.js

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './app.css';
 import Root from './root';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
